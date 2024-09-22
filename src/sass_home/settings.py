58755a615f135ecf88eabc6307e52f96a23df7d7
TheSoftNode <thesoftnode@gmail.com>
2024-09-22 09:28:23 +0100

    Add packages to handle postgresql connections

--- src/sass_home/settings.py
+++ src/sass_home/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--i0r8*d$n&ycya%ad#w_z6+^k!xn)vxd1b7f-d+e6%0wi0jn1)'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas_frame.prod.railway.app
